b9b5a556f7bfc7290254848e9179ff20c10d0de8
Diana Olmedo <diana.olmedo@estudiant.enti.cat>
2025-12-11 23:20:31 +0100

    peo acabat

--- Ajedrez_Diana_Roc_AA4/Ajedrez_Diana_Roc_AA4/movement.cpp
+++ Ajedrez_Diana_Roc_AA4/Ajedrez_Diana_Roc_AA4/movement.cpp
@@ -8,7 +8,7 @@ bool isPawnMoveValid(int fromRow, int fromCol, int toRow, int toCol)
 {
     char piece = getPiece(fromRow, fromCol);
 
-    if (fromRow < 0 && fromRow >= BOARD_SIZE || toCol < 0 && toCol >= BOARD_SIZE) {
+    if (fromRow < 0 || fromRow >= BOARD_SIZE || toRow < 0 && toRow >= BOARD_SIZE || fromCol < 0 || fromCol >= BOARD_SIZE || toCol < 0 || toCol >= BOARD_SIZE) {
 
         return false;
     }
@@ -20,20 +20,72 @@ bool isPawnMoveValid(int fromRow, int fromCol, int toRow, int toCol)
 
     bool isWhite = (piece = whitePawn); //Dirección: -1 para BLANCAS
     int direction;
+    int startRow;
+
     if (isWhite)
     {
         direction = -1;
+        startRow = 6;
     }
     else
     {
         direction = 1;
+        startRow = 1;
     }
 
+    int deltaR = toRow - fromRow;
+    int deltaC_raw = toCol - fromCol;
+    int deltaC;
 
 
+    if (deltaC_raw < 0) {
+        deltaC = -deltaC_raw;
+    }
+    else {
+        deltaC = deltaC_raw;
+    }
 
+    char destinationPiece = getPiece(toRow, toCol);
 
-    
+   //solo permitimos avance recto
+    if (deltaC != 0) {
+        return false;
+    }
+
+    //revisamos que la diferencia de fila sea en la dirección correcta
+    if (deltaR * direction < 0) {
+        return false;
+    }
+
+    // Avance de un casillero
+    if (deltaR == direction) {
+        // Debe estar vacío
+        if (destinationPiece == emptySpace) {
+            return true;
+        }
+    }
+
+    // Avance de dos casilleros (solo desde la fila inicial)
+    else if (deltaR == (2 * direction)) {
+        // 1. Debe estar en la fila de inicio
+        if (fromRow != startRow) {
+            return false;
+        }
+
+        // 2. El destino y el casillero intermedio deben estar vacíos
+        int intermediateRow = fromRow + direction;
+
+        // Verificación de dos casilleros vacíos
+        if (getPiece(intermediateRow, fromCol) == emptySpace) {
+            if (destinationPiece == emptySpace) {
+                return true;
+            }
+        }
+    }
+
+    // Si no es un movimiento de 1 ni 2 casilleros, es inválido.
+    return false;
+}
 
    
      
